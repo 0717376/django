a1342e30578e076deea431b6ce11942ec2800a8a
sergey <0717376@gmail.com>
2022-07-26 15:37:20 +0300

    commit

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+from .models import Post, Tag
+
 
 # Create your views here.
 def posts_list(request):
-    n = ['Sergey', 'Dasha', 'Pasha', 'Masha']
-    return render(request, 'blog/index.html', context ={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html/', context={'post': post})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags':tags})
